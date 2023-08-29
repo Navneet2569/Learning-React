d3f218c54ee772d3b441e0e2c5d2a3f9f721e896
Navneet2569 <nav9027388401singh@gmail.com>
2023-08-29 07:34:37 -0700

    Commit

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,11 +1,13 @@
 import react from "react";
 import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Welcome />
     </div>
   );
 }
