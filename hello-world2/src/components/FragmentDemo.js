971de83c1b4bc1c01891e5ef0595fe31e381ebd2
Navneet2569 <nav9027388401singh@gmail.com>
2023-09-07 22:30:46 -0700

    Fragments continued with passing key values

--- hello-world2/src/components/FragmentDemo.js
+++ hello-world2/src/components/FragmentDemo.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function FragmentDemo() {
   return (
-    <div>
+    <React.Fragment>
       <h1>Fragment Demo</h1>
       <p>This describes the Fragment Demo component</p>
-    </div>
+    </React.Fragment>
   );
 }
 
