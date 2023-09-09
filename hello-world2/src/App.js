fa2cb529ec49d6be26cb0724255dc8c7e93b084c
Navneet2569 <nav9027388401singh@gmail.com>
2023-09-08 21:49:06 -0700

    Pure Component

--- hello-world2/src/App.js
+++ hello-world2/src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import "./App.css";
 import FragmentDemo from "./components/FragmentDemo";
 import Table from "./components/Table";
+import PureComp from "./components/PureComp";
+import ParentComp from "./components/ParentComp";
 
 function App() {
   return (
     <div className="App">
-      <FragmentDemo />
-      <Table />
+      {/* <FragmentDemo /> */}
+
+      {/* <Table /> */}
+
+      <ParentComp />
     </div>
   );
 }
