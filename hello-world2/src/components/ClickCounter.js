8e282f2078494690b0f309cfd5a71f60b2638862
Navneet2569 <nav9027388401singh@gmail.com>
2023-09-13 22:34:23 +0530

    Designing Reusable Components

--- hello-world2/src/components/ClickCounter.js
+++ hello-world2/src/components/ClickCounter.js
@@ -2,27 +2,11 @@ import React, { Component } from "react";
 import UpdatedComponent from "./withCounter";
 
 class ClickCounter extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      count: 0,
-    };
-  }
-
-  clickHandler = () => {
-    this.setState((prevState) => {
-      return { count: prevState.count + 1 };
-    });
-  };
   render() {
-    const { count } = this.state;
+    const { count, incrementCount } = this.props;
     return (
       <div>
-        <button onClick={this.clickHandler}>
-          {" "}
-          {this.props.name} Clicked {count} times
-        </button>
+        <button onClick={incrementCount}> Clicked {count} times</button>
       </div>
     );
   }
--- hello-world2/src/components/HoverCounter.js
+++ hello-world2/src/components/HoverCounter.js
@@ -2,26 +2,11 @@ import React, { Component } from "react";
 import UpdatedComponent from "./withCounter";
 
 class HoverCounter extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      count: 0,
-    };
-  }
-  hoverHandler = () => {
-    this.setState((prevState) => {
-      return { count: prevState.count + 1 };
-    });
-  };
   render() {
-    const { count } = this.state;
+    const { count, incrementCount } = this.props;
     return (
       <div>
-        <h1 onMouseOver={this.hoverHandler}>
-          {" "}
-          {this.props.name} Hovered {count} times
-        </h1>
+        <h1 onMouseOver={incrementCount}> Hovered {count} times</h1>
       </div>
     );
   }
