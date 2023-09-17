0b7540f59acbd8daf824bf143bf062460db53e8a
Navneet2569 <nav9027388401singh@gmail.com>
2023-09-17 21:34:47 +0530

    Context Type and Default Values

--- hello-world2/src/components/E.js
+++ hello-world2/src/components/E.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import F from "./F";
+import UserContext from "./UserContext";
 class E extends Component {
+  static contextType = UserContext;
   render() {
     return (
       <div>
+        Component E context {this.context}
         <F />
       </div>
     );
   }
 }
+
+// E.contextType = UserContext;
 
 export default E;
