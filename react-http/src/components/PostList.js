aafbbbd423e3a61b5f712bdc317875c772da1be7
Navneet2569 <nav9027388401singh@gmail.com>
2023-09-18 21:55:12 +0530

    Axios

--- react-http/src/components/PostList.js
+++ react-http/src/components/PostList.js
@@ -7,6 +7,7 @@ class PostList extends Component {
 
     this.state = {
       posts: [],
+      errorMsg: "",
     };
   }
   componentDidMount() {
@@ -18,16 +19,18 @@ class PostList extends Component {
       })
       .catch((error) => {
         console.log(error);
+        this.setState({ errorMsg: "Error Retrieving Data" });
       });
   }
   render() {
-    const { posts } = this.state;
+    const { posts, errorMsg } = this.state;
     return (
       <div>
         List of Posts
         {posts.length
           ? posts.map((post) => <div key={post.id}> {post.title} </div>)
           : null}
+        {errorMsg ? <div>{errorMsg}</div> : null}
       </div>
     );
   }
