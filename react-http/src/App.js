06ad0f665e4173feec420fbe098255f222818efa
Navneet2569 <nav9027388401singh@gmail.com>
2023-09-19 22:17:05 +0530

    Get and Post using Axios

--- react-http/src/App.js
+++ react-http/src/App.js
@@ -5,7 +5,7 @@ import PostList from "./components/PostList";
 function App() {
   return (
     <div className="App">
-      {/* <PostList /> */}
+      <PostList />
 
       <PostForm />
     </div>
