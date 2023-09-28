cf6d755551b9e0ce67be9b4ab7d2a4c162e511ea
Navneet2569 <nav9027388401singh@gmail.com>
2023-09-28 21:41:23 +0530

    useEffect with incorrect deps

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -6,6 +6,7 @@ import HookCounter2 from "./components/HookCounter2";
 import HookCounter3 from "./components/HookCounter3";
 import HookCounter1 from "./components/HookCounter1";
 import HookMouse from "./components/HookMouse";
+import IntervalHookCounter from "./components/IntervalHookCounter";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       {/* <HookCounter3 /> */}
       {/* <HookCounter1 /> */}
       <HookMouse />
+      <IntervalHookCounter />
     </div>
   );
 }
