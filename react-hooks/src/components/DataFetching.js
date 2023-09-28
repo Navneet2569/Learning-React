dd16890e1240450142669fcda847c5150818e97e
Navneet2569 <nav9027388401singh@gmail.com>
2023-09-28 22:29:00 +0530

    Making Fetch button

--- react-hooks/src/components/DataFetching.js
+++ react-hooks/src/components/DataFetching.js
@@ -4,9 +4,10 @@ import axios from "axios";
 function DataFetching() {
   const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [id2, setId2] = useState(1);
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${id2}`)
       .then((res) => {
         console.log(res);
         setPost(res.data);
@@ -14,10 +15,17 @@ function DataFetching() {
       .catch((err) => {
         console.log(err);
       });
-  }, [id]);
+  }, [id2]);
+
+  const handleClick = () => {
+    setId2(id);
+  };
   return (
     <div>
       <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>
+        Fetch Post
+      </button>
       {/* <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
